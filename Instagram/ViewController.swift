de0be1e4feb063da9ae8b83d1072be905a4123c2
Christopher Mena <mena.christopher.student@gmail.com>
2021-03-12 16:57:17 -0500

    Initial files

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Christopher Mena on 3/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(":Hello")
+    }
+
+
+}
+
