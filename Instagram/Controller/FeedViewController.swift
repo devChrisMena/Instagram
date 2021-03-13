009cfdd5737aa36b8976a111e83d91067cfe5de2
Christopher Mena <mena.christopher.student@gmail.com>
2021-03-12 22:34:33 -0500

    Added extra functionality

--- Instagram/Controller/FeedViewController.swift
+++ Instagram/Controller/FeedViewController.swift
@@ -11,21 +11,53 @@ import AlamofireImage
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
-    
+    let myRefreshControl = UIRefreshControl()
     var posts = [PFObject]()
+    var numberOfPosts: Int!
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        myRefreshControl.addTarget(self, action: #selector(loadPosts), for: .valueChanged)
         // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
+        loadPosts()
+    }
+    
+    
+    @IBAction func onLogOut(_ sender: UIBarButtonItem) {
+        PFUser.logOut()
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    @objc func loadPosts() {
+        numberOfPosts = 20
         let query = PFQuery(className: "Posts")
         query.includeKey("author")
-        query.limit = 20
+        query.limit = numberOfPosts
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                for post in posts! {
+                    self.posts.append(post)
+                }
+                self.tableView.reloadData()
+            } else {
+                print("Error \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    //Infinite scroll, load more posts
+    func loadMorePosts() {
+        numberOfPosts += 20
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = numberOfPosts
         
         query.findObjectsInBackground { (posts, error) in
             if posts != nil {
@@ -34,6 +66,12 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             } else {
                 print("Error \(error?.localizedDescription)")
             }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == posts.count {
+            loadMorePosts()
         }
     }
     
--- Instagram/Controller/LoginViewController.swift
+++ Instagram/Controller/LoginViewController.swift
@@ -7,14 +7,23 @@
 
 import UIKit
 import Parse
+import SwiftHEXColors
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    
+    var buttonColor = [false, false]
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        usernameField.tag = 0
+        passwordField.tag = 1
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
     
     @IBAction func onSignIn(_ sender: UIButton) {
@@ -23,7 +32,10 @@ class LoginViewController: UIViewController {
         
         PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
             if user != nil {
+                
                 self.performSegue(withIdentifier: "loginSegue", sender: self)
+                self.usernameField.text = ""
+                self.passwordField.text = ""
             } else {
                 print("Error: \(error?.localizedDescription)")
             }
@@ -43,6 +55,28 @@ class LoginViewController: UIViewController {
             }
         }
     }
-
-
+    
+    @IBAction func textFiedlChanged(_ sender: UITextField) {
+        let colorLogin = UIColor(hexString: "#0095F6")
+        let colorEmpty = UIColor(hexString: "B7DEFC")
+        if sender.text != "" {
+            buttonColor[sender.tag] = true
+        }
+        if buttonColor[0] && buttonColor[1] {
+            logInButton.backgroundColor = colorLogin
+        }
+        else {
+            logInButton.backgroundColor = colorEmpty
+        }
+    }
+    @IBAction func onReveal(_ sender: UIButton) {
+        if passwordField.isSecureTextEntry == true {
+            passwordField.isSecureTextEntry = false
+            sender.setBackgroundImage(UIImage(named: "eye"), for: .normal)
+        } else {
+            passwordField.isSecureTextEntry = true
+            sender.setBackgroundImage(UIImage(named: "eye.slash"), for: .normal)
+        }
+    }
+    
 }
